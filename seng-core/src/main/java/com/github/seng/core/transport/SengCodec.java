0825403ac5bc636eb83de7a8d088d5debbe1182b
wangyongxu <wangyx1122@gmail.com>
2020-09-27 23:53:59 +0800

    feat(core): codec

--- seng-core/src/main/java/com/github/seng/core/transport/SengCodec.java
+++ seng-core/src/main/java/com/github/seng/core/transport/SengCodec.java
@@ -3,10 +3,10 @@ package com.github.seng.core.transport;
 import com.github.seng.core.exception.SengRuntimeException;
 import com.github.seng.core.serialize.Serializer;
 import com.github.seng.core.serialize.SerializerFactory;
+import com.github.seng.core.utils.ClassUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
@@ -15,6 +15,12 @@ import java.io.*;
  * @author wangyongxu
  */
 public class SengCodec implements Codec {
+
+    public static final byte REQUEST_FLAG = 0x00;
+    public static final byte RESPONSE_FLAG = 0x10;
+    public static final byte ONE_WAY = 0x20;
+    public static final byte PING = 0x30;
+    public static final byte PONG = 0x40;
 
     @Override
     public void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws IOException {
@@ -117,7 +123,7 @@ public class SengCodec implements Codec {
     /**
      * 解码服务端的响应
      */
-    private Object decodeResponse(byte[] body, Serializer serializer) throws IOException, ClassNotFoundException {
+    private Object decodeResponse(byte[] body, Serializer serializer) throws IOException {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(body);
         ObjectInput input = createInput(inputStream);
         String serviceName = input.readUTF();
@@ -127,7 +133,25 @@ public class SengCodec implements Codec {
 
 
     @Override
-    public Object decode(Channel channel, ByteBuf input) throws IOException {
-        return null;
+    public Object decode(Channel channel, ByteBuf input) throws Exception {
+        SengProtocolHeader header = new SengProtocolHeader();
+        header.setVersion(input.readByte());
+        byte b = input.readByte();
+        header.setMsgType((byte) ((b & 240) >>> 4));
+        header.setSerializerId((byte) (b & 15));
+        b = input.readByte();
+        header.setStatusCode((byte) ((b & 248) >>> 3));
+        header.setReqId(input.readLong());
+        int bodyLength = input.readInt();
+        header.setDataLength(bodyLength);
+        byte[] body = new byte[bodyLength];
+        input.getBytes(input.readerIndex(), body, 0, bodyLength);
+        Serializer serializer = SerializerFactory.getSerializer(header.getSerializerId());
+        if (header.getMsgType() == RESPONSE_FLAG) {
+            return decodeResponse(body, serializer);
+        } else if (header.getMsgType() == REQUEST_FLAG) {
+            return decodeRequest(body, serializer);
+        }
+        throw new IllegalArgumentException("not support msg type: " + header.getMsgType());
     }
 }
