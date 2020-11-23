71b7a1847a8d64f40c52849a06b5f8b7f3670a2e
wangyongxu <wangyx1122@gmail.com>
2020-11-23 09:21:20 +0800

    feat(rpc): 删除rpc

--- seng-core/src/main/java/com/github/seng/core/rpc/ExportHandler.java
+++
@@ -1,9 +0,0 @@
-package com.github.seng.core.rpc;
-
-/**
- * @author wangyongxu
- */
-public class ExportHandler {
-
-
-}
--- seng-core/src/main/java/com/github/seng/core/rpc/ProxyFactory.java
+++
@@ -1,9 +0,0 @@
-package com.github.seng.core.rpc;
-
-/**
- * @author wangyongxu
- */
-public interface ProxyFactory {
-
-    <T> T getProxy(Invoker<T> invoker, Class<?>[] interfaces);
-}
